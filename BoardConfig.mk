7b20282432dd8ff76a7f8875a44b8159c7d216fb
dianlujitao <dianlujitao@lineageos.org>
2017-11-19 11:17:04 +0800

    deen: Nuke BOARD_USES_QC_TIME_SERVICES flag
     * Any pre-O framework did exact the same thing as TimeService.apk
       when the flag is enabled, as we always ship TimeService.apk,
       there's no reason to keep the flag, and the framework stuff is
       not ported to O.
     * Remove unused 32-bit library as well.
    
    Change-Id: I74abc65468e983732bccde571003235891379463

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,9 +231,6 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
-# Timeservice
-BOARD_USES_QC_TIME_SERVICES := true
-
 # Treble
 BOARD_VNDK_VERSION := current
 

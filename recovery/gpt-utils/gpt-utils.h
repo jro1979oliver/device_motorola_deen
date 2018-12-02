b533b8084fedbc210a42e4f6caa7dcefa9262aa5
Alberto97 <albertop2197@gmail.com>
2018-12-02 14:27:23 +0100

    Revert "gpt-utils: Add xbl_config to the list of A/B partitions"
    This reverts commit b78b8a3d8fcfb9b5700932171fb284477dce8b55.

--- recovery/gpt-utils/gpt-utils.h
+++ recovery/gpt-utils/gpt-utils.h
@@ -79,8 +79,7 @@ extern "C" {
 #define AB_SLOT_A_SUFFIX                "_a"
 #define AB_SLOT_B_SUFFIX                "_b"
 #define PTN_XBL                         "xbl"
-#define PTN_XBL_CFG                     "xbl_config"
-#define PTN_SWAP_LIST                   PTN_XBL, PTN_XBL_CFG, "sbl1", "rpm", "tz", "aboot", "hyp", "lksecapp", "keymaster", "cmnlib", "cmnlib32", "cmnlib64", "pmic", "apdp", "devcfg", "hosd", "keystore", "msadp", "abl", "mdtpsecapp", "mdtp"
+#define PTN_SWAP_LIST                   PTN_XBL, "sbl1", "rpm", "tz", "aboot", "hyp", "lksecapp", "keymaster", "cmnlib", "cmnlib32", "cmnlib64", "pmic", "apdp", "devcfg", "hosd", "keystore", "msadp", "abl", "mdtpsecapp", "mdtp"
 #define AB_PTN_LIST PTN_SWAP_LIST, "boot", "system", "oem", "modem", "bluetooth", "dsp"
 #define BOOT_DEV_DIR    "/dev/block/bootdevice/by-name"
 

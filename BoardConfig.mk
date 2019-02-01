87ea74d8da1b1e1f78ddb820da9b81f5075348a6
Tri Vo <trong@google.com>
2019-02-01 11:27:25 -0800

    deen: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
    We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
    extends system sepolicy. To that end, we use another make variable to
    specify product sepolicy.
    
    Bug: 119305624
    Test: m selinux_policy
    Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -229,7 +229,7 @@ VENDOR_SECURITY_PATCH := 2021-10-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current

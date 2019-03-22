a63db5bf5e3fc041ebf82e12709f8fdccdf2634c
Vladimir Oltean <olteanv@gmail.com>
2019-03-22 22:37:05 +0000

    deen: Sync extractors with templates
     * This introduces fixup scripts and kang mode and fixes
       passing of arguments longer than one word,
       such as --section "Hello world".
    
    Change-Id: I038d64c8d3b55c26bb1b9798cc67b9e8ad37e292

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -29,73 +29,70 @@ CLEAN_VENDOR=true
 KANG=
 SECTION=
 
-while [ "$1" != "" ]; do
-    case "$1" in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -k | --kang)            KANG="--kang"
-                                ;;
-        -s | --section )        shift
-                                SECTION="$1"
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC="$1"
-                                ;;
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
 if [ -z "${SRC}" ]; then
-    SRC=adb
+    SRC="adb"
 fi
 
-# Load wrapped shim
 function blob_fixup() {
     case "${1}" in
-
-    vendor/lib/libmot_gpu_mapper.so)
-        sed -i "s/libgui/libwui/" "${2}"
-        ;;
-
-    vendor/lib/libmmcamera_vstab_module.so)
-        sed -i "s/libgui/libwui/" "${2}"
-
-        ;;
-
-    # Fix missing symbols
-    vendor/lib64/libril-qc-hal-qmi.so)
-        patchelf --add-needed libcutils_shim.so "${2}"
-        ;;
-
-# memset shim
-    vendor/bin/charge_only_mode)
-        patchelf --add-needed libmemset_shim.so "${2}"
-        ;;
-
-    vendor/lib64/libril-qc-hal-qmi.so)
-        patchelf --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
-        ;;
-
-    vendor/lib64/libwvhidl.so)
-        patchelf --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
-
-        ;;
-    # Fix camera recording
-    vendor/lib/libmmcamera2_pproc_modules.so)
-        sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
-        ;;
-
+        # Fix camera recording
+        vendor/lib/libmmcamera2_pproc_modules.so)
+            sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
+            ;;
+        # Wrap libgui_vendor into libwui
+        vendor/lib/libmot_gpu_mapper.so)
+            sed -i "s/libgui/libwui/" "${2}"
+            ;;
+        vendor/lib/libmmcamera_vstab_module.so)
+            sed -i "s/libgui/libwui/" "${2}"
+            ;;
+        # Use libprotobuf-cpp-lite-v29.so for libwvhidl.so
+        vendor/lib64/libwvhidl.so)
+            patchelf --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+            ;;
+        # Missing symbols for ril
+	vendor/lib64/libril-qc-hal-qmi.so)
+	    patchelf --add-needed libcutils_shim.so "${2}"
+	    ;;
+        # Use libprotobuf-cpp-lite-v29.so for libril-qc-hal-qmi.so
+        vendor/lib64/libril-qc-hal-qmi.so)
+            patchelf --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+            ;;
+        # memset shim
+	vendor/bin/charge_only_mode)
+	    patchelf --add-needed libmemset_shim.so "${2}"
+	    ;;
     esac
 }
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
-for i in $(grep -rn 'libhidltransport.so\|libhwbinder.so' ../../../vendor/motorola/deen/proprietary | awk '{print $3}'); do
+"${MY_DIR}/setup-makefiles.sh"
+
+# Remove libhwbinder - libhidltransport depedencies
+for i in $(grep -rn 'libhidltransport.so\|libhwbinder.so' ../../../vendor/motorola/"${DEVICE}"/proprietary | awk '{print $3}'); do
 	patchelf --remove-needed "libhwbinder.so" "$i"
 	patchelf --remove-needed "libhidltransport.so" "$i"
 done
-
-"${MY_DIR}/setup-makefiles.sh"

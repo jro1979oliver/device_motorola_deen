fb63d83977663828b7f81179b01b9c34c14e79f8
Erfan Abdi <erfangplus@gmail.com>
2019-08-15 19:35:44 +0430

    deen: Add NoCutoutOverlay
    Change-Id: I667a76f149f6c3bcfeb95244838afaf05ce0d259

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # A/B updater

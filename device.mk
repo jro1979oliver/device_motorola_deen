a01d94bd9691bab49c77e86db2832c7a574cb1b8
Baldev Sahu <bsahu@codeaurora.org>
2020-07-09 16:40:46 +0530

    deen: Update display config to version 1.9
    * upgrade display config version to 1.9.
    * explicitly build it
    
    Change-Id: I7fdd3d527ba732897e4fe91849996b895bc88037

--- device.mk
+++ device.mk
@@ -169,7 +169,8 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libqdMetaData.system \
     libvulkan \
-    libtinyxml
+    libtinyxml \
+    vendor.display.config@1.9
 
 # DRM
 PRODUCT_PACKAGES += \

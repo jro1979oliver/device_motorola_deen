5309cbedf283a18776bc430dd0c1700ddf3cc04c
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    deen: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- device.mk
+++ device.mk
@@ -321,6 +321,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
 # QCOM
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \

96d697e5f1fb050c1f5070488d7e39256b16f1ef
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-01-28 10:54:53 +0200

    deen: Add some product propert overrides

--- omni_deen.mk
+++ omni_deen.mk
@@ -23,6 +23,14 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit from deen device
 $(call inherit-product, device/motorola/deen/device.mk)
 
+# Platform
+TARGET_BOARD_PLATFORM := msm8953
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=$(TARGET_BOARD_PLATFORM) \
+    ro.hardware.bootctrl=$(TARGET_BOARD_PLATFORM) \
+    ro.vendor.build.security_patch=2099-12-31 \
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := deen
 PRODUCT_NAME := omni_deen

3102680e66c0bf90620a6bb5069bd1d012c327f4
100Daisy <100Daisy@gmail.com>
2021-02-04 11:04:48 +0100

    deen: Enable debugging
    set TW_DEBUG_BUILD flag to true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,12 @@ TW_NEW_ION_HEAP := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 
+# Debugging (trip the flag)
+ifeq ($(strip $(TW_DEBUG_BUILD)),)
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+endif
+
 TARGET_RECOVERY_DEVICE_MODULES += \
 	android.hardware.boot@1.0 \
     libicuuc \

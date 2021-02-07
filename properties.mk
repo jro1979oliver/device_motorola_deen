47f96709eacfbf184d52ce73988d767f150bfa3a
TogoFire <italomellopereira@gmail.com>
2021-02-07 19:05:21 +0000

    deen: Import some camera props to fix camera crashing
    Change-Id: Ied347fa4ff53a8eed213786f2b98f0fdd1ace8ea

--- properties.mk
+++ properties.mk
@@ -60,8 +60,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     vidc.enc.dcvs.extra-buff-count=2 \
-    persist.vendor.camera.preview.ubwc=0 \
-    vendor.video.disable.ubwc=1
+    media.camera.ts.monotonic=1 \
+    persist.vendor.camera.display.lmax=1280x720 \
+    persist.vendor.camera.display.umax=1920x1080 \
+    vendor.camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.instagram.android \
+    vendor.camera.lowpower.record.enable=1 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.android.GoogleCamera \
+    vendor.camera.aux.packagelist2=com.android.systemui,com.huaqin.cameraautotest,com.huaqin.runtime \
+    vendor.camera.aux.packageblacklist=com.discord \
+    persist.vendor.qti.telephony.vt_cam_interface=2 \
+    persist.vendor.camera.dual.camera=0 \
+    persist.vendor.camera.eis.enable=1 \
+    persist.vendor.camera.gyro.disable=0 \
+    persist.vendor.camera.isp.clock.optmz=0 \
+    persist.vendor.camera.stats.test=5 \
+    persist.vendor.camera.CDS=off \
+    persist.camera.HAL3.enabled=1
 
 # Codec2 switch
 PRODUCT_PROPERTY_OVERRIDES += \

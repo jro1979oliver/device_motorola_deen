0c9973633e4821c95bfca25e449bfbdaa4e37109
Jeferson <jroliveira.oliveira301@gmail.com>
2021-03-01 18:24:58 +0100

    deen: Aicipify
    Change-Id: I7c35056d3dc38675ad15ea3203d3b8ad7ffd9415

--- device.mk
+++ device.mk
@@ -33,8 +33,7 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay

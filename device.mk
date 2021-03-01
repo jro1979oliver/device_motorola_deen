54db248d9a8c22eab587f87845ab7df75fd62f5e
Jeferson <jroliveira.oliveira301@gmail.com>
2021-03-01 18:24:58 +0100

    deen: Aicipify
    Change-Id: I7c35056d3dc38675ad15ea3203d3b8ad7ffd9415

--- device.mk
+++ device.mk
@@ -29,8 +29,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 

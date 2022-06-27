e0762768b24d854d415502b6299ba7156e410430
jro1979oliver <jroliveira.oliveira301@gmail.com>
2022-06-27 14:43:45 -0300

    deen: Use prebuilt 4.0 audio
    * Move telephony props to vendor.prop
    
    Change-Id: I8c38cf96cc73a0ec53707fbbf055a12de9de614a

--- device.mk
+++ device.mk
@@ -68,14 +68,17 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@4.0-impl_32 \
+    android.hardware.audio.service_32 \
+    android.hardware.audio@4.0-impl_32 \
+    android.hardware.soundtrigger@2.1-impl_32 \
+    android.hardware.soundtrigger@2.1-service \
     audio.a2dp.default \
-    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    audiod \
     libaacwrapper \
+    libaudio-resampler \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
@@ -106,7 +109,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     libbt-vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.fm@1.0.vendor
@@ -244,6 +247,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qti.qseecomd.sh \
     init.hidl.sensor.rc \
+    init.mmi.acdb.sh \
     init.mmi.chipset.rc \
     init.mmi.overlay.rc \
     init.mmi.usb.rc \

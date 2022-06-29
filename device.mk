b3ae7fc6bcd505aaaf68b39e74c52cbf28e203fa
Jeferson Oliveira <jroliveira.oliveira301@gmail.com>
2022-06-29 13:46:51 -0300

    deen: exclude audiofx
    Change-Id: I9c833858eb81cdd476727e31a555453928ae4cea

--- device.mk
+++ device.mk
@@ -105,6 +105,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
